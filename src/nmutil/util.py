22bb7e4a3ec7d5f6139ed0bd427609af33ddafb3
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-05-01 14:32:21 +0100

    add note about flatten function (one already exists in nmigen)

--- src/nmutil/util.py
+++ src/nmutil/util.py
@@ -1,6 +1,7 @@
 from collections.abc import Iterable
 
-
+# XXX this already exists in nmigen._utils
+# see https://bugs.libre-soc.org/show_bug.cgi?id=297
 def flatten(v):
     if isinstance(v, Iterable):
         for i in v:
