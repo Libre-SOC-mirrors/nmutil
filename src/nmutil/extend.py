f74008fcc2ce1525e919e4062f9d49506726edf6
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2020-05-20 14:28:03 +0100

    add sign/zero extending utilities

---
+++ src/nmutil/extend.py
@@ -0,0 +1,17 @@
+from nmigen import Repl, Cat, Const
+
+
+def exts(exts_data, width, fullwidth):
+    exts_data = exts_data[0:width]
+    topbit = exts_data[-1]
+    signbits = Repl(topbit, fullwidth-width)
+    return Cat(exts_data, signbits)
+
+
+def extz(exts_data, width, fullwidth):
+    exts_data = exts_data[0:width]
+    topbit = Const(0)
+    signbits = Repl(topbit, fullwidth-width)
+    return Cat(exts_data, signbits)
+
+
