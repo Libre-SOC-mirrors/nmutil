740a02e64970937e68a04dbe3a71e4111868ddb7
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2021-02-21 15:27:40 +0000

    extra comments in byte_reverse function

--- src/nmutil/byterev.py
+++ src/nmutil/byterev.py
@@ -6,7 +6,7 @@
 
 from nmigen import Signal, Cat
 
-# TODO: turn this into a module
+# TODO: turn this into a module?
 def byte_reverse(m, name, data, length):
     """byte_reverse: unlike nmigen word_select this takes a dynamic length
 
@@ -16,6 +16,7 @@ def byte_reverse(m, name, data, length):
     comb = m.d.comb
     data_r = Signal.like(data, name=name)
 
+    # if length is a static integer, we do not require a Case statement
     if isinstance(length, int):
         j = length
         rev = []
@@ -25,6 +26,7 @@ def byte_reverse(m, name, data, length):
         comb += data_r.eq(Cat(*rev))
         return data_r
 
+    # Switch statement needed: dynamic length had better be = 1,2,4 or 8
     with m.Switch(length):
         for j in [1,2,4,8]:
             with m.Case(j):
@@ -33,5 +35,4 @@ def byte_reverse(m, name, data, length):
                     rev.append(data.word_select(j-1-i, 8))
                 comb += data_r.eq(Cat(*rev))
     return data_r
-
 
